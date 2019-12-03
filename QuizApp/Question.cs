7cd39775ce88d745fa3c9a15a190cd279962fb93
Omasri59 <omar59@live.nl>
2019-12-03 15:46:38 +0100

    Add project files.

---
+++ QuizApp/Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizApp
+{
+     class Question
+    {
+        public string question { get; set; } 
+        public string answer { get; set; }
+        ///yuir
+    }
+}
